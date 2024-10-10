67ea9bb6746f90e063e7c06939fc827f6040a0f4
derahhayes <dermot.hayes@derahsoftware.com>
2024-10-10 19:49:05 +0100

    Updates Settings.py

--- SCP/SCP/settings.py
+++ SCP/SCP/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -48,10 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-
     #dermots apps 
-    'members'
-    
+    'members',
     ]
 
 MIDDLEWARE = [
