c99e65f77d0a62610dcd025f1d9f81e9252d0a57
derahhayes <dermot.hayes@derahsoftware.com>
2024-10-10 18:12:59 +0100

    Test Commit

--- SCP/SCP/views.py
+++ SCP/SCP/views.py
@@ -6,15 +6,13 @@ def homepage(request, *args, **kwargs):
    # return HttpResponse("Hello World")
    member.objects.create()
    queryset = member.objects.all()
-   my_title = "Swim Club PAL"
-   
+   my_title = "Swim Club matey"
    my_context = {
        
        "page_title": my_title,
        "members_count": queryset.count()
        
    }
-   
    return render(request, 'home.html', my_context)
 
 def about(request, *args, **kwargs):
