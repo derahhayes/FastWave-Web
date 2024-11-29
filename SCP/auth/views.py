52d65d8a250c205587c5552f865cd933a851b01b
derahhayes <dermot.hayes@derahsoftware.com>
2024-11-29 12:29:33 +0000

    Fixed after Webstorm broke it

--- SCP/auth/views.py
+++ SCP/auth/views.py
@@ -27,7 +27,7 @@ def register_view(request):
         # username_exists = User.objects.filter(username__iexact=username).exists()
         # email_exists = User.objects.filter(username__iexact=username).exists()
     try:
-        user.objects.create_user(username, email=email, password=password)
+        User.objects.create_user(username, email=email, password=password)
     except:
         pass
     return render(request, "auth/register.html", {})
